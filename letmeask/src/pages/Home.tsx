4c4d39b261365ed1617f864649cbe21e49cd9308
gjoao98 <gjoao98@gmail.com>
2021-06-24 14:50:22 -0300

    Refatorando o projeto e crindo o hook

--- letmeask/src/pages/Home.tsx
+++ letmeask/src/pages/Home.tsx
@@ -1,18 +1,17 @@
 import { useHistory } from 'react-router-dom'
-import { useContext } from 'react'
 
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import googleIconImg from '../assets/images/google-icon.svg'
 
-import { AuthContext } from '../App'
+import { useAuth } from '../hooks/Auth'
 import { Button } from '../components/Button'
 
 import '../styles/auth.scss'
 
 export function Home() {
     const history = useHistory();
-    const { user, signInWithGoogle } = useContext(AuthContext);
+    const { user, signInWithGoogle } = useAuth();
 
     async function handleCreateRoom() {
         if (!user) {
